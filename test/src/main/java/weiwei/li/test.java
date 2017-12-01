0c5f60f08187bb54da5656d4ab2e4c869e7a8a67
VisitorsSeeTheSea <447014777@qq.com>
2017-12-01 14:07:24 +0800

    hello

--- test/src/main/java/weiwei/li/test.java
+++ test/src/main/java/weiwei/li/test.java
@@ -1,7 +1,12 @@
 package weiwei.li;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 /**
  * Created by weiwei.li on 2017/12/1.
  */
 public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
 }
